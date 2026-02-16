ed0b32dff5662bf0e656ab81a6bace833d68999e
kumar.krish07022002@gmail.com <kumar.krish07022002@gmail.com>
2026-02-16 15:17:42 +0530

    Two Sum

--- Array/TwoSum.cpp
+++ Array/TwoSum.cpp
@@ -32,11 +32,31 @@ class Solution {
                     res[0] = i;
                     res[1] = j;
                     return res;
-                    
                 }
             }
         }
         return res;
+    }
+}
+*/
+
+
+/*
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            int complement = target - nums[i];
+
+            if(mp.containsKey(complement)){
+                return new int[]{mp.get(complement), i};
+            }
+
+            mp.put(nums[i], i);
+
+        }
+        throw new RuntimeException("No soln. found");
     }
 }
 */
